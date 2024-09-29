e574f080b882e34e90dfc2578c4b7d0bf6715547
muhammadhassaan693 <muhammad.hassaan.khurram@gmail.com>
2024-09-29 10:39:56 +0300

    lesson8

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#dictionary with intiger keys
+mydictionary={1:"apple",
+            2:"PINEAPPLE",
+            3:"grapes"}
+
+print(mydictionary)
+
+#accessing the data using the key
+print(mydictionary[2])
