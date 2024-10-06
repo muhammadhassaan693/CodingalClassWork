55d57fd3eb9a5883e5e166e178bf9c9c01a6b80b
muhammadhassaan693 <muhammad.hassaan.khurram@gmail.com>
2024-10-06 10:49:20 +0300

    lesson10

---
+++ library management.py
@@ -0,0 +1,35 @@
+class library:
+    def __init__(self, list_of_book, nm):
+        #initializing the variable or fethure in this class
+        self.booklist=list_of_book
+        self.name=nm
+        self.lenthOfDictionary={}
+
+    def displayBooks(self):
+        print(f"we have the following books in our library = {self.name}")
+
+        for book in self.booklist:
+            print(book)
+
+
+    def lendBook(self, user, book):
+        if book not in self.booklist:
+            print("sorry we do not have that book")
+        elif book in self.lenthOfDictionary:
+            print("the book is already being used by ",self.lenthOfDictionary[book])
+        else:
+            self.lenthOfDictionary[book]= user       
+
+    def addBook(self,book):
+        self.booklist.append(book)
+        print(f"{book} as been added to the book list")
+
+
+
+books = library(
+
+['Python', 'Rich Dad Poor Dad', 'Harry Potter', 'C++ Basics', 'Algorithms by CLRS'],
+
+"Let's Upskill")
+
+userName=input("please enter your name: ")
