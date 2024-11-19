2c17bfd774ff0f515e0a5c8e41abdc3f1a9f37d1
muhammadhassaan693 <muhammad.hassaan.khurram@gmail.com>
2024-11-19 18:20:17 +0300

    binary_to_decimal

---
+++ binary_to_decimal.py
@@ -0,0 +1,8 @@
+# Get binary input from the user
+binary_input = input("Enter your Binary: ")
+
+# Convert binary to decimal
+decimal_output = int(binary_input, 2)
+
+# Print the decimal output
+print("Decimal:", decimal_output)
