a11063955555b5db8019945985449291090a9f68
muhammadhassaan693 <muhammad.hassaan.khurram@gmail.com>
2024-11-30 10:48:34 +0300

    lesson 25

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,8 +1,21 @@
-# Get binary input from the user
-binary_input = input("Enter your Binary: ")
+#function to convert from binary to decimal
+def binary_to_decimal(binary):
+    decimal = 0
+    i = 0
 
-# Convert binary to decimal
-decimal_output = int(binary_input, 2)
+    while binary != 0:
+        temp = binary % 10
+        decimal = decimal + temp * pow(2,i)
+        binary = binary // 10
+
+        i = i + 1
+
+    return decimal
+
+# Get binary input from the user
+binary_input = int(input("Enter your binary number = "))
+
+decimal_output = binary_to_decimal(binary_input)
 
 # Print the decimal output
 print("Decimal:", decimal_output)
---
+++ implement_bit_wise.py
@@ -0,0 +1,9 @@
+first_num = 36
+second_num = 12
+
+print(f"first number is {first_num}")
+print(f"second number is {second_num}")
+
+print()
+
+print(f"first number AND second number = {first_num & second_num}")
